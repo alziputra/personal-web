ff9096c6b561c11f510f221a1c0a54c87372380a
alziputra <alziputra12@gmail.com>
2023-10-04 21:13:13 +0700

    update seluruh file

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// JavaScript untuk mengaktifkan tombol navigasi
+document.getElementById("menu-icon").addEventListener("click", function () {
+    var navList = document.querySelector(".navlist");
+    navList.classList.toggle("active");
+});
+
 // Dapatkan semua elemen dengan class "progress-bar"
 const skillBars = document.querySelectorAll(".progress");
 
@@ -13,7 +19,7 @@ const experiences = [{
         title: "Cloud Computing",
         company: "Bangkit Academy 2023 batch 1 - Graduate",
         duration: "Feb 2023 - Jul 2023",
-        description: "building an application to help cocoa farmers carry out early diagnosis of the condition of their cocoa plants by uploading photos of their cocoa plants. The system will then classify cocoa diseases based on the uploaded photos, and provide information on how to prevent and treat the identified diseases. Developed by the C23-PC638 team which consists of 6 people who contribute in their respective fields. The machine learning team is tasked with creating an algorithm that is used to predict types of disease in cocoa plants. The cloud computing team is responsible for building relationships between the machine learning team and the mobile development team. Meanwhile, mobile development is responsible for creating interfaces and interactions with users. This project is part of our final Capstone Project at Bangkit Academy 2023 led by Google, GoTo, and Traveloka"
+        description: "Building an application to help cocoa farmers carry out early diagnosis of the condition of their cocoa plants by uploading photos of their cocoa plants. The system will then classify cocoa diseases based on the uploaded photos, and provide information on how to prevent and treat the identified diseases. Developed by the C23-PC638 team which consists of 6 people who contribute in their respective fields. The machine learning team is tasked with creating an algorithm that is used to predict types of disease in cocoa plants. The cloud computing team is responsible for building relationships between the machine learning team and the mobile development team. Meanwhile, mobile development is responsible for creating interfaces and interactions with users. This project is part of our final Capstone Project at Bangkit Academy 2023 led by Google, GoTo, and Traveloka"
     },
     {
         title: "Software Engineer",
@@ -34,7 +40,7 @@ function renderExperience() {
         const title = document.createElement("h3");
         title.textContent = experience.title;
 
-        const company = document.createElement("p");
+        const company = document.createElement("span");
         company.textContent = experience.company;
 
         const duration = document.createElement("p");
